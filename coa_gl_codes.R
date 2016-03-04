380615a3729502f0a11a250ba04c2e09a46077b3
Jay Fisher <fisher.jc@gmail.com>
2016-03-04 11:22:47 -0500

    adding to chart of accounts R file

--- coa_gl_codes.R
+++ coa_gl_codes.R
@@ -37,7 +37,7 @@
 # Install packages
 library(dplyr)
 
-# Set working directory where chart of accounts is
+# Set working directory where you save the CSVs
 
 # Read in gl_code.csv
 gl <- read.csv(file = "gl_code.csv", header = F)
@@ -71,3 +71,46 @@ gl_4 <- filter(gl_3, `Account Code` >= 6010, Description != 'accounting use only
 
 # Write csv called fa_gl_upload_[insert date]
 write.csv(gl_4, file = paste("fa_gl_upload_", format(Sys.Date(), "%Y-%m-%d"), ".csv"), row.names = F)
+
+#### Type 3 ####
+# Most likely, no changes in this Chart of Accounts sheet.
+# Manually look at Excel file and compare with TrackVia.
+
+#### Dept 4 ####
+# Most likely, no changes in this Chart of Accounts sheet.
+# Manually look at Excel file and compare with TrackVia to see if there are updates
+# Keep rows with Account Title values ending in "Program" or with no "- *" at the end of value
+
+# Column Names, Excel
+  # Account Code 
+  # Account Title
+  # Account Short Title
+
+# Save Dept 4 as CSV and read in sheet
+dept <- read.csv(file = "dept.csv", header = F)
+
+# Remove the six rows before column headers row
+dept <- dept[-c(1:6),]
+
+# Remove empty columns, V4:V7
+dept$V4 <- NULL
+dept$V5 <- NULL
+dept$V6 <- NULL
+dept$V7 <- NULL
+
+# Turn column headers row into actual column names
+dept_2 <- rename(dept, 
+                 "Account Code" = V1,
+                 "Account Title" = V2,
+                 "Account Short Title" = V3)
+
+# Drop rows with current column names
+dept_3 <- dept_2[-1,]
+
+# Convert Account Code to number, Account Title and Account Short Title to characters
+dept_3$`Account Code` <- as.numeric(as.character(dept_3$`Account Code`))
+dept_3$`Account Title` <- as.character(dept_3$`Account Title`)
+dept_3$`Account Short Title` <- as.character(dept_3$`Account Short Title`)
+
+# Remove Account Title with "- Admin", "INACTIVE", or blank
+
